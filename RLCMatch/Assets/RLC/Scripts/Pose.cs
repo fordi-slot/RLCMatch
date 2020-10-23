e0f346bd9cc83a042b849802505a33eddf644e73
Harsh Priyadarshi <harsh.priyadarshi@fordi.in>
2020-10-23 17:12:30 +0530

    Runtime clip adjustments disabled

--- RLCMatch/Assets/RLC/Scripts/Pose.cs
+++ RLCMatch/Assets/RLC/Scripts/Pose.cs
@@ -165,11 +165,11 @@ namespace RLC.Animation
 
         private void PlayClip(UnityEngine.Animation animation, AnimationClip clip)
         {
-            clip.legacy = true;
-            if (clip.name.ToLower().Contains("begin") || clip.name.ToLower().Contains("orgasm"))
-                clip.wrapMode = WrapMode.Once;
-            else
-                clip.wrapMode = WrapMode.Loop;
+            //clip.legacy = true;
+            //if (clip.name.ToLower().Contains("begin") || clip.name.ToLower().Contains("orgasm"))
+            //    clip.wrapMode = WrapMode.Once;
+            //else
+            //    clip.wrapMode = WrapMode.Loop;
 
             //clip.wrapMode = WrapMode.Loop;
             if (animation.GetClip(clip.name) == null)
